9f440528453c9a2c162d5d6bd9b0c465d641f577
João Silva <jsilva@e-goi.com>
2021-01-15 17:26:14 +0000

    Initial commit

---
+++ EgoiPushLibrary/EgoiPushLibrary/EgoiPushLibrary.h
@@ -0,0 +1,18 @@
+//
+//  EgoiPushLibrary.h
+//  EgoiPushLibrary
+//
+//  Created by João Silva on 15/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EgoiPushLibrary.
+FOUNDATION_EXPORT double EgoiPushLibraryVersionNumber;
+
+//! Project version string for EgoiPushLibrary.
+FOUNDATION_EXPORT const unsigned char EgoiPushLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EgoiPushLibrary/PublicHeader.h>
+
+
